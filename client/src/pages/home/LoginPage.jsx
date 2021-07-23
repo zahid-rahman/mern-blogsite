c6253fbfc2389963ff6ea585a24e9bf96d17a8d0
corbin009 <rahmanzahid298@gmail.com>
2021-07-24 02:01:49 +0600

    FIX:(client) fixed login validation problem

--- client/src/pages/home/LoginPage.jsx
+++ client/src/pages/home/LoginPage.jsx
@@ -1,17 +1,31 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-import { Col, Container, Row, Form, Button } from 'react-bootstrap'
+import { Col, Container, Row, Form, Button, Alert } from 'react-bootstrap'
 import { Link, useHistory } from 'react-router-dom'
 import PageTitle from '../../components/head-title/PageTitle'
-import { getCookie, setCookie } from '../../utils/loginSession'
+import { setCookie } from '../../utils/loginSession'
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API
 
-const LoginPage = (props) => {
-    const [userRequest, setUserRequest] = useState({})
+const LoginPage = () => {
+    const [userRequest, setUserRequest] = useState({
+        email: "",
+        password: ""
+    })
+    const [error, setError] = useState({});
+    const [alertVisible, setAlertVisible] = useState(false)
+    const [statusCode, setStatusCode] = useState(0)
     const history = useHistory()
     const bigScreenCustomSize = {
         span: 4,
         offset: 4
+    }
+
+    const showAlert = () => {
+        setAlertVisible(true)
+        setTimeout(() => {
+            setAlertVisible(false);
+        }, 6000);
+
     }
 
     const changeHandler = (event) => {
@@ -29,14 +43,17 @@ const LoginPage = (props) => {
             const response = await axios.post(`${SERVER_API_URL}/user/login`, userRequest)
             console.log(response.data)
             setCookie(response.data)
-            // window.location.href="/dashboard"
             history.push('/dashboard')
-            
+
         }
         catch (error) {
             console.error(error)
+            setError(error.response.data)
+            setStatusCode(error.response.status)
         }
     }
+
+    const { email, password } = error;
 
     return (
         <Container>
@@ -49,29 +66,40 @@ const LoginPage = (props) => {
                     <Form onSubmit={loginSubmitHandler}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Control
+                                className={email ? "form-control is-invalid" : "form-control"}
                                 name="email"
                                 ype="email"
                                 placeholder="Enter email"
                                 onChange={changeHandler}
                             />
+                            <p className="invalid-feedback">{email}</p>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Control
+                                className={password ? "form-control is-invalid" : "form-control"}
                                 type="password"
                                 placeholder="Password"
                                 name="password"
                                 onChange={changeHandler}
                             />
+                            <p className="invalid-feedback">{password}</p>
+
                         </Form.Group>
                         <Form.Group className="mb-3 text-center" controlId="formBasicCheckbox">
                             <Link to='/' className="btn btn-link">back to homepage</Link>
                             <Link to='/signup' className="btn btn-link">Not registered yet</Link>
                         </Form.Group>
-                        <Button variant="primary" type="submit" className="w-100">
+                        <Button variant="primary" type="submit" className="w-100" onClick={showAlert}>
                             Sign Up
                         </Button>
                     </Form>
+                    <br />
+                    {statusCode === 401 ?
+                        <Alert variant="danger" show={alertVisible} className="text-center p-3"><b>wrong email and password</b></Alert>
+                        : ""
+                    }
+
                 </Col>
             </Row>
         </Container>
--- client/src/pages/home/SignUpPage.jsx
+++ client/src/pages/home/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
