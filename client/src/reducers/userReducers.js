6eff913909b5f947530734c9d5bca6981dd7243a
zahid-rahman <rahmanzahid94@gmail.com>
2022-01-14 12:10:00 +0600

    WIP:restructured whole project and seperate client in another repository

--- client/src/reducers/userReducers.js
+++
@@ -1,8 +0,0 @@
-export const saveUserDetailsReducer = (state = {}, action) => {
-    switch (action.type) {
-        case 'SAVE_LOGGEDIN_USER_DETAILS':
-            return state = action.payload
-        default:
-            return state;
-    }
-}
